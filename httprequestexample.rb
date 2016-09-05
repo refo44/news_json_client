6ba87f5acff3a4bee6300dcc21f0f11cc3f13d61
refo44 <refo44@gmail.com>
2016-09-05 04:58:49 -0400

    add user-agent for reddit request
    Se modifico la manera de realizar la consulta con NET::HTTP para poder
    especificar el user-agent debido a que reddit lo pide

---
+++ httprequestexample.rb
@@ -0,0 +1,39 @@
+system("clear")
+
+require 'net/http'
+require 'json' 
+
+system('clear')
+
+uri = URI('https://www.reddit.com/.json')
+
+params = {limit: 1}  # incluimos mediante hash parametros en el query 
+#uri.query = URI.encode_www_form(params)
+
+my_request = Net::HTTP::Get.new(uri)
+my_request["User-Agent"] = "Refo\'s News Reader" #Especificamos un "User-Agent"
+
+resp = Net::HTTP.start(uri.hostname) {|http|        
+	http.request(my_request)
+}
+
+resp_hash = JSON.parse(resp.body)  #if resp.is_a?(Net::HTTPSuccess)
+
+puts resp_hash.class
+puts resp_hash.length
+
+reddit_news = resp_hash["data"]["children"]
+
+puts "************************************"
+puts "Cantidad de noticias obtenidas: #{reddit_news.length}"
+puts "************************************"
+
+reddit_news.each_with_index { |reddit_new, i|
+	puts "#{i + 1}.- #{reddit_new['data']['title']}"
+}
+
+puts
+
+puts
+
+  p reddit_news.first["data"]["author"]
